2eb958a40fe5fac33842e2dda3f5cfa9f8a1c8d6
Martins Sipenko <martins.sipenko@gmail.com>
2015-09-25 22:04:25 +0300

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@
 cd build
 ls -la
 echo $FTP_USER
-find . -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD "$FTP_HOST" \;
+echo $FTP_PASSWORD
+find . -type f -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD $FTP_HOST{} \;
 cd -
 
